cdff2fd9cf65b9fcd3128b469c8a9fa558c6179a
Thomas Borowski <mail@thomasborowski.de>
2020-07-16 07:30:39 +0200

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,8 @@
+<?php
+require_once(__DIR__ . "/lang/language.php");
+$l = new Lang("de-DE");
+?>
+
+<h1><?= $l->t("general.text1") ?></h1>
+<p><?= $l->t("general.group1.text1") ?></p>
+<p><?= $l->t("more.another") ?></p>
